dabc0aa209ec154fc21c7a3ecf06a248aec7bd8c
Nguyen Phong <nguyenphong0210pt@gmail.com>
2023-11-25 18:23:37 +0700

    add full source

--- src/main.cpp
+++ src/main.cpp
@@ -12,5 +12,12 @@ void setup(void) {
 
 void loop() {
   asbQuadcopter_step();
+  Serial.print(motors_outport[0]);
+  Serial.print("  ");
+  Serial.print(motors_outport[1]);
+  Serial.print("  ");
+  Serial.print(motors_outport[2]);
+  Serial.print("  ");
+  Serial.println(motors_outport[3]);
   delay(10);
 }
